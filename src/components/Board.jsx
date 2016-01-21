3f26702b9d857f04bdfdb462ee309cc021899350
Hubert Wesołowski <hwesol13@gmail.com>
2016-01-21 10:51:00 +0100

    Almost ready app

---
+++ src/components/Board.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Container from './Container'
+
+export default React.createClass({
+
+  displayName: 'Dashboard',
+
+  render() {
+    return (
+      <Container>
+        Hello worlddadadada!
+      </Container>
+    );
+  }
+});
---
+++ src/utils/connection.js
@@ -0,0 +1,4 @@
+import Syncano from 'syncano';
+var connection = new Syncano({apiKey: '887e79a13201a795318d1372135847857789f6f7', instance: 'throbbing-voice-1318'});
+
+export default connection;
